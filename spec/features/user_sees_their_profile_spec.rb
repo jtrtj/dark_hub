7e56a1437bb35d729c078fe13dccfabaabf7b67d
jtrtj <jt.r@live.com>
2018-08-23 14:55:02 -0600

    add expectation for commits to be in view

--- spec/features/user_sees_their_profile_spec.rb
+++ spec/features/user_sees_their_profile_spec.rb
@@ -37,4 +37,20 @@ feature 'registered user visits their dashboard' do
     # expect(page).to have_css('.repo_description')
     # expect(page).to have_css('.repo_link')
   end
+
+  scenario 'they see a list of their commits', :vcr do
+    user = User.create!(
+                        provider: 'github',
+                        uid: ENV['jtrtj_test_uid'],
+                        name: 'John Roemer',
+                        token: ENV['jtrtj_test_token'],
+                        user_name: 'jtrtj'
+                      )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    expect(page).to have_css('.commits')
+  end
 end
