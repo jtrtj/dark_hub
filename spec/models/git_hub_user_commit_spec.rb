cf5ca4702ecbba24cbd054f919573715db292488
jtrtj <jt.r@live.com>
2018-08-23 23:24:02 -0600

    pass commit model test

--- spec/models/git_hub_user_commit_spec.rb
+++ spec/models/git_hub_user_commit_spec.rb
@@ -1,4 +1,15 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 describe GitHubUserCommit do
+  it 'has attributes' do
+    attributes = JSON.parse(File.read("fixtures/sample_repo.json"), symbolize_names: true)
+
+    commit = GitHubUserCommit.new(attributes)
+
+    expect(commit.message).to eq(attributes[:commit][:message])
+    expect(commit.html_url).to eq(attributes[:commit][:html_url])
+    expect(commit.repo_name).to eq(attributes[:repository][:name])
+    expect(commit.repo_html_url).to eq(attributes[:repository][:html_url])
+    expect(commit.date).to eq(attributes[:commit][:author][:date])
+  end
 end
